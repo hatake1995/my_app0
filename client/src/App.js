22fb9f7358c829c714072cd1883916556683201c
hatake1995 <tasaka103@yahoo.co.jp>
2023-05-18 12:23:34 +0900

    5/18

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Pagebar from "./components/Pagebar";
+
+function App() {
+  return (
+    <div className="App">
+      <Pagebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Pagebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Pagebar = () => {
+    return (
+        <div className='pagebar'>
+            <div className="home">
+                <li>HOME</li>
+            </div>
+            <div className="youshi">
+                <li>画用紙</li>
+            </div>
+            <div className="fusen">
+                <li>付箋</li>
+            </div>
+        </div>
+    )
+}
+
+export default Pagebar;
